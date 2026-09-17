aa6bcbb234be63e59eccb79007c931623b49a3f1
oliver <oliver.quail3@gmail.com>
2026-09-17 10:22:10 +1000

    Updated font size for complexity_combined

--- complexity_combined.py
+++ complexity_combined.py
@@ -17,7 +17,7 @@ non_reasoning = df.loc[df["Model"] == "Mistral"]
 
 
 
-fig, ax = plt.subplots(figsize=(6,16), ncols=1, nrows=3)
+fig, ax = plt.subplots(figsize=(12,18), ncols=1, nrows=3)
 
 df_cleaned_reasoning_faithfulness = reasoning.dropna(subset=['Faithfulness'])
 df_cleaned_non_reasoning_faithfulness = non_reasoning.dropna(subset=['Faithfulness'])
@@ -33,13 +33,13 @@ labels = ["Q-0-S", "M-0-S", "Q-0-C", "M-0-C", "Q-C-S", "M-C-S", "Q-C-C", "M-C-C"
 
 # Row one
 ax1 = ax[0].boxplot([df_cleaned_reasoning_faithfulness.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Zero")]["Faithfulness"], df_cleaned_non_reasoning_faithfulness.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Zero")]["Faithfulness"], df_cleaned_reasoning_faithfulness.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Zero")]["Faithfulness"], df_cleaned_non_reasoning_faithfulness.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Zero")]["Faithfulness"], df_cleaned_reasoning_faithfulness.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Context")]["Faithfulness"], df_cleaned_non_reasoning_faithfulness.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Context")]["Faithfulness"], df_cleaned_reasoning_faithfulness.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Context")]["Faithfulness"], df_cleaned_non_reasoning_faithfulness.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Context")]["Faithfulness"] ], tick_labels=labels, patch_artist=True)
-ax[0].set_title("Faithfulness")
+ax[0].set_title("Faithfulness", fontsize=16)
 
 ax2 = ax[1].boxplot([df_cleaned_reasoning_coherence.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Zero")]["Faithfulness"], df_cleaned_non_reasoning_coherence.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Zero")]["Coherence"], df_cleaned_reasoning_coherence.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Zero")]["Coherence"], df_cleaned_non_reasoning_coherence.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Zero")]["Coherence"], df_cleaned_reasoning_coherence.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Context")]["Coherence"], df_cleaned_non_reasoning_coherence.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Context")]["Coherence"], df_cleaned_reasoning_coherence.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Context")]["Coherence"], df_cleaned_non_reasoning_coherence.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Context")]["Coherence"] ], tick_labels=labels, patch_artist=True)
-ax[1].set_title("Coherence")
+ax[1].set_title("Coherence", fontsize=16)
 
 ax3 = ax[2].boxplot([df_cleaned_reasoning_dq.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Zero")]["Decision Quality"], df_cleaned_non_reasoning_dq.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Zero")]["Decision Quality"], df_cleaned_reasoning_dq.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Zero")]["Decision Quality"], df_cleaned_non_reasoning_dq.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Zero")]["Decision Quality"], df_cleaned_reasoning_dq.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Context")]["Decision Quality"], df_cleaned_non_reasoning_dq.loc[(df["Scenario"] == "Simple") & (df["Strategy"] == "Context")]["Decision Quality"], df_cleaned_reasoning_dq.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Context")]["Decision Quality"], df_cleaned_non_reasoning_dq.loc[(df["Scenario"] == "Complex") & (df["Strategy"] == "Context")]["Decision Quality"] ], tick_labels=labels, patch_artist=True)
-ax[2].set_title("Decision Quality")
+ax[2].set_title("Decision Quality", fontsize=16)
 
 
 colours = ["purple", "orange", "purple", "orange", "purple", "orange", "purple", "orange"]
@@ -58,6 +58,12 @@ ax[0].tick_params(axis='x', labelrotation=90)
 ax[1].tick_params(axis='x', labelrotation=90)
 ax[2].tick_params(axis='x', labelrotation=90)
 
+ax[0].tick_params(axis="x", labelsize=16)
+ax[0].tick_params(axis="y", labelsize=16)
+ax[1].tick_params(axis="x", labelsize=16)
+ax[1].tick_params(axis="y", labelsize=16)
+ax[2].tick_params(axis="x", labelsize=16)
+ax[2].tick_params(axis="y", labelsize=16)
 
 
 ax[0].text(
@@ -66,7 +72,7 @@ ax[0].text(
     transform=ax[0].transAxes,      
     verticalalignment='bottom',   
     horizontalalignment='right',  
-    fontsize=12,
+    fontsize=16,
     color='Black'
 )
 
@@ -76,7 +82,7 @@ ax[1].text(
     transform=ax[1].transAxes,      
     verticalalignment='bottom',   
     horizontalalignment='right',  
-    fontsize=12,
+    fontsize=16,
     color='Black'
 )
 
@@ -86,7 +92,7 @@ ax[2].text(
     transform=ax[2].transAxes,      
     verticalalignment='bottom',   
     horizontalalignment='right',  
-    fontsize=12,
+    fontsize=16,
     color='Black'
 )
 
